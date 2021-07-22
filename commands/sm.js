fa53b9c7d9dd7de18b4a62c7d52c1db68017c0b3
GABSIS06 <86327730+GABSIS06@users.noreply.github.com>
2021-07-23 01:16:26 +0200

    Add files via upload

---
+++ commands/sm.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'sm',
+    description: 'Social Media Command !',
+        
+    async execute(message, args){
+// inside a command, event listener, etc.
+        const exampleEmbed = new Discord.MessageEmbed()
+	    .setColor('#RANDOM')
+	    .setTitle("GABSIS SOCIAL MEDIA")
+	    .setAuthor('*Social Media*', 'https://imgur.com/jTmrinT.gif', '')
+	    .setDescription('**How can we help you?**')
+		.addFields(
+			{ name: 'DISCORD', value: '!                    G A B S I S#1978 [ <@595755770054705152> ]', inline: false },
+		)
+		.addField(`INSTAGRAM`, `[gabsis._](https://www.instagram.com/gabsis._)` , true)
+		.addFields(
+			{ name: '--------------------------------------------------------', value: '*', inline: false},
+		
+		)
+		.addField('YouTube', `[My YouTube Channel](https://www.youtube.com/c/GABSIS06)`, true)
+		.setURL('https://discord.gg/ZWPmgmnNz6')
+		.setThumbnail()
+	    .setImage('https://imgur.com/rhlWI3h.gif')
+	    .setTimestamp()
+	    .setFooter("© GABSIS", "https://imgur.com/D06I1DN.gif");
+
+    messageEmbed = await message.channel.send(exampleEmbed)
+    }
+}
