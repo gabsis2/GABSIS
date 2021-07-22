071d5a7af8f25f4412add885d184ead965add98a
GABSIS06 <86327730+GABSIS06@users.noreply.github.com>
2021-07-23 01:12:47 +0200

    Add files via upload

---
+++ commands/gabsis.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'gabsis',
+    description: "this is a command!",
+    execute(message, args){
+        message.channel.send('<a:GABSIS:853248873254223922> Ouaa zabour :sunglasses: stannah tw k yji yjawbak :slight_smile:');
+
+        
+
+
+    }
+}
