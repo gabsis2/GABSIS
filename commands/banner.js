ad779deef92a09ac4e1f5e8c46d55c31abd1e4e4
GABSIS06 <86327730+GABSIS06@users.noreply.github.com>
2021-07-23 01:09:39 +0200

    Add files via upload

---
+++ commands/banner.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'banner',
+    description: 'Banner Command !',
+        
+    async execute(message, args){
+// inside a command, event listener, etc.
+        const exampleEmbed = new Discord.MessageEmbed()
+	    .setColor('#RANDOM')
+	    .setTitle("*GABSIS BANNER*")
+		.setURL('https://imgur.com/0C9oirR.jpg')
+		.setThumbnail()
+	    .setImage('https://imgur.com/0C9oirR.jpg')
+	    .setTimestamp()
+	    .setFooter("© GABSIS", "https://imgur.com/D06I1DN.gif");
+
+    messageEmbed = await message.channel.send(exampleEmbed)
+    }
+}
